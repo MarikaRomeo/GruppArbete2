ce3b4645f7d92c43fc6df9ee489102f666093fa8
Ironboy <ironboy@users.noreply.github.com>
2025-10-27 15:04:26 +0100

    with instructions

--- main.js
+++ main.js
@@ -21,14 +21,46 @@ for (let { firstName, lastName, email } of people) {
   `;
 }*/
 
-// Using map instead of a loop
-let html = people.map(({ firstName, lastName, email }) => `
-    <div class="person">
+function render(search = '') {
+
+  // A modern programming pattern (in many languages)
+  // is using a chain of array methods to calculate a result
+  // filter, storting, mapping until we're happy :)
+
+  // Using map instead of a loop
+  // to convert our array of objects (people) to html
+  let html = people
+    // return true on filtering if search is an empty string
+    // or firstName starts with the search
+    .filter(({ firstName }) => search === ''
+      || firstName.toLowerCase().startsWith(search.toLowerCase()))
+    // sort by firstName
+    .toSorted((a, b) => a.firstName > b.firstName ? 1 : - 1)
+    // map to convert each element to a string with html
+    .map(({ firstName, lastName, email }) => `
+    <section class="person">
       <p><b>First name:</b> ${firstName}</p>
       <p><b>Last name:</b> ${lastName}</p>
       <p><b>Email:</b> ${email}</p>
-    </div>
-`);
+    </section>
+  `)
+    // join to join our array of strings into one large string
+    .join('');
+
+  // replace the content of article.people element with our new html
+  document.querySelector('.people').innerHTML = html;
+
+}
+
+// add a keyup event handler to our search field
+document.querySelector('.search-field')
+  .addEventListener('keyup', event => {
+    // the event object has a target the html element
+    // that trigger the event and all input element have a value
+    render(event.target.value);
+  });
 
 
-document.body.innerHTML = html;
+// initial rendering of list of people to screen
+render();
+
